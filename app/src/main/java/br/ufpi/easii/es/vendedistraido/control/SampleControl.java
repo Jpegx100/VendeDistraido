93c0ab060a7cd74c380e511c7879ea6467bb2641
Jpegx100 <jpegx100@gmail.com>
2016-06-22 13:55:18 -0300

    New Packages
    New packages and files add

---
+++ app/src/main/java/br/ufpi/easii/es/vendedistraido/control/SampleControl.java
@@ -0,0 +1,7 @@
+package br.ufpi.easii.es.vendedistraido.control;
+
+/**
+ * Created by jpegx on 6/22/2016.
+ */
+public class SampleControl {
+}
---
+++ app/src/main/java/br/ufpi/easii/es/vendedistraido/exception/SampleException.java
@@ -0,0 +1,7 @@
+package br.ufpi.easii.es.vendedistraido.exception;
+
+/**
+ * Created by jpegx on 6/22/2016.
+ */
+public class SampleException {
+}
