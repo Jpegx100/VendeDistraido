1ce2e852a8bf1df6c915e92a432fd692ab6efed0
otaviocury <otaviocury.oc@gmail.com>
2016-06-30 14:52:59 -0300

    Telas
    Telas iniciais

--- app/src/main/java/br/ufpi/easii/es/vendedistraido/view/MainActivity.java
+++ app/src/main/java/br/ufpi/easii/es/vendedistraido/view/MainActivity.java
@@ -2,18 +2,82 @@ package br.ufpi.easii.es.vendedistraido.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import br.ufpi.easii.es.vendedistraido.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText email, senha;
+    private Button btnEntrar, btnCadastrar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email = (EditText) findViewById(R.id.main_edt_email);
+        senha = (EditText) findViewById(R.id.main_edt_senha);
+
+        btnCadastrar = (Button) findViewById(R.id.main_btn_cadastrar);
+        btnEntrar = (Button) findViewById(R.id.main_btn_entrar);
+
+        btnCadastrar.setOnClickListener(onClickCadastrar());
+
+        btnEntrar.setOnClickListener(onClickEntrar());
+
     }
-    //teste
-    //Testando Novamente
-    //gutao
-    //cury
+
+    private View.OnClickListener onClickEntrar(){
+        return new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+        };
+    }
+
+    private View.OnClickListener onClickCadastrar(){
+        return new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+        };
+    }
+
+
+    public EditText getEmail() {
+        return email;
+    }
+
+    public void setEmail(EditText email) {
+        this.email = email;
+    }
+
+    public EditText getSenha() {
+        return senha;
+    }
+
+    public void setSenha(EditText senha) {
+        this.senha = senha;
+    }
+
+    public Button getBtnEntrar() {
+        return btnEntrar;
+    }
+
+    public void setBtnEntrar(Button btnEntrar) {
+        this.btnEntrar = btnEntrar;
+    }
+
+    public Button getBtnCadastrar() {
+        return btnCadastrar;
+    }
+
+    public void setBtnCadastrar(Button btnCadastrar) {
+        this.btnCadastrar = btnCadastrar;
+    }
 }
