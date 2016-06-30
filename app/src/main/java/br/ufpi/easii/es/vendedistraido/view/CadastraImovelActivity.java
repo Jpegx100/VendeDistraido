7c4ad7a346c3a6eef32587cbb323df9ebcf62e86
Jpegx100 <jpegx100@gmail.com>
2016-06-30 14:29:23 -0300

    Layouts de Telas
    Layouts e Activities das telas usadas na primeira iteração

---
+++ app/src/main/java/br/ufpi/easii/es/vendedistraido/view/CadastraImovelActivity.java
@@ -0,0 +1,15 @@
+package br.ufpi.easii.es.vendedistraido.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.ufpi.easii.es.vendedistraido.R;
+
+public class CadastraImovelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastra_imovel);
+    }
+}
