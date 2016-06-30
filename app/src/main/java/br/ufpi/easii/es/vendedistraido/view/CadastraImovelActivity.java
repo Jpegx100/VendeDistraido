c46231a8124fd795932492567db87818abf2c1f8
Jpegx100 <jpegx100@gmail.com>
2016-06-30 16:06:07 -0300

    Alteracao das telas
    alteradas algumas telas

--- app/src/main/java/br/ufpi/easii/es/vendedistraido/view/CadastraImovelActivity.java
+++ app/src/main/java/br/ufpi/easii/es/vendedistraido/view/CadastraImovelActivity.java
@@ -2,14 +2,32 @@ package br.ufpi.easii.es.vendedistraido.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import br.ufpi.easii.es.vendedistraido.R;
 
 public class CadastraImovelActivity extends AppCompatActivity {
-
+    private EditText edt_titulo, edt_latitude, edt_longitude, edt_endereco;
+    private Button btn_cadastrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastra_imovel);
+        edt_titulo = (EditText)findViewById(R.id.cadastra_imovel_edt_titulo);
+        edt_latitude = (EditText)findViewById(R.id.cadastra_imovel_edt_latitude);
+        edt_longitude = (EditText)findViewById(R.id.cadastra_imovel_edt_longitude);
+        edt_endereco = (EditText)findViewById(R.id.cadastra_imovel_edt_endereco);
+        btn_cadastrar = (Button)findViewById(R.id.cadastra_imovel_btn_cadastrar);
+        btn_cadastrar.setOnClickListener(onClickCadastrar());
+    }
+    private View.OnClickListener onClickCadastrar(){
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        };
     }
 }
--- app/src/main/java/br/ufpi/easii/es/vendedistraido/view/GestorActivity.java
+++ app/src/main/java/br/ufpi/easii/es/vendedistraido/view/GestorActivity.java
@@ -6,26 +6,49 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import br.ufpi.easii.es.vendedistraido.R;
 
 public class GestorActivity extends AppCompatActivity {
-
+    private Button btn_corretores, btn_imoveis, btn_relatorios;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gestor);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        btn_corretores = (Button)findViewById(R.id.gestor_btn_corretores);
+        btn_corretores.setOnClickListener(onClickCorretores());
+        btn_imoveis = (Button)findViewById(R.id.gestor_btn_imoveis);
+        btn_imoveis.setOnClickListener(onClickImoveis());
+        btn_relatorios = (Button)findViewById(R.id.gestor_btn_relatorios);
+        btn_relatorios.setOnClickListener(onClickRelatorios());
     }
 
+    private View.OnClickListener onClickCorretores() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        };
+    }
+
+    private View.OnClickListener onClickImoveis() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        };
+    }
+
+    private View.OnClickListener onClickRelatorios() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        };
+    }
 }
